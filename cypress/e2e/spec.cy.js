68ddae84acc23f738ffd9629a2acbdfac49cc49e
ylan <hei.nalisoa@gmail.com>
2023-02-22 14:55:13 +0300

    feat: test e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,14 +3,26 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 
-  it('contains Kitchen',()=>{
+  xit('contains Kitchen',()=>{
     cy.visit('https://example.cypress.io').contains('Kitchen')
     cy.contains("children").click();
     cy.url().should('contains' , 'traversal');
   })
 
-  xit("contains input todo" , ()=>{
+  it("contains input todo" , ()=>{
     cy.visit('http://localhost:3000')
     cy.get('#add-todo').type("Lorem ipsum");
+  })
+  it("contains input todo after enter" , ()=>{
+    let text = "Lorem ipsum"
+    cy.visit('http://localhost:3000')
+    cy.get('#add-todo').type('todo{enter}');
+    cy.get('[data-testid="test-todo-0-container"]').contains("todo")
+  })
+  it("card done most have text todo",()=>{
+    cy.visit('http://localhost:3000')
+    cy.get('#add-todo').type('todo{enter}');
+    cy.get('[data-testid="test-todo-0"]').check();
+    cy.get('[data-testid="test-todo-0-container"]').contains("todo")
   })
 })
--- src/Input.jsx
+++ src/Input.jsx
@@ -2,6 +2,6 @@ export function Input(props) {
     const { onChange, onKeyUp } = props;
 
     return (
-        <input id='add-todo' type="text" onChange={onChange} onKeyUp={onKeyUp}/>
+        <input type="text" id="add-todo" onChange={onChange} onKeyUp={onKeyUp}/>
     );
 }
